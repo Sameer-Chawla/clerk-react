fab27d3248486833ee7ed01ad66d825f4ca9d9f2
Sameer Chawla <92089327+Sameer-Chawla@users.noreply.github.com>
2024-10-21 21:35:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // or wherever your API server is running
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
